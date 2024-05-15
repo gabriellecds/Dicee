edf7cc679629407d536f5a1a9ad19a8cd731d865
Gabrielle <gabrielle97oliveira@gmail.com>
2024-05-15 16:36:28 +0100

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Gabrielle Oliveira on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
